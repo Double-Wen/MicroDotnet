58e9964a8bb496b806e11a5c5e327f4eccc48ae6
Double-Wen <tcliuwenwen@126.com>
2020-05-02 23:28:09 +0800

    fix comment of convert type

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double d = 12.34;
+            int i = (int)d;
+            Console.WriteLine(d);
+            Console.WriteLine(i);
+            Int64 a = 1;
+            Int32 b = (Int32)a; //显式类型转化，可能会有数据丢失
+            Int64 c = b;    //隐式类型转化，安全的，从小整型转化为大整型
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+        }
+    }
+}
