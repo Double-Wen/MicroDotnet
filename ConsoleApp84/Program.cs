b800bd2ed003afafb46f56d6e872f3057a5959f2
Double-Wen <tcliuwenwen@126.com>
2020-06-16 00:21:41 +0800

    fix 84 and 85

--- ConsoleApp84/Program.cs
+++ ConsoleApp84/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 namespace ConsoleApp84
 {
+
     class Program
     {
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> aaa = new List<int>();
+            Random ddd = new Random();
+            for (int i = 0; i < 10; i++) aaa.Add(ddd.Next(1, 10));
+
+            Func<int> f1 = () => DateTime.Now.Year;
+            Func<int, double> f2 = (int r) => Math.Sqrt(r);
+            Func<int, int, double> f3 = (int a, int b) => Math.Sqrt(a * a + b * b);
+            Predicate<int> s1 = (int a) => a > 5 ? true : false;
+            Console.WriteLine("{0} {1} {2} {3}", f1(), f2(5), f3(3, 4), s1(ddd.Next(0, 10)));
+
+            Action<string> d1 = (string a) => Console.WriteLine(a);
+            d1("123");
+
+            Action<int, List<int>> d2 = (int a, List<int> bbb) => Console.WriteLine(a >= bbb[3] ? "大" : "小");
+            d2(5, aaa);
+
+
         }
+
     }
 }
+
--- ConsoleApp85/Program.cs
+++ ConsoleApp85/Program.cs
@@ -1,12 +1,72 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
 namespace ConsoleApp85
 {
-    class Program
-    {
-        static void Main(string[] args)
+    //资料类的定义省略
+    
+
+    static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            IList<Racer> racers = Racer.GetChampions();
+            FileStream fs = File.Create("E:555.txt");
+            StreamWriter sw = new StreamWriter(fs);
+            for (int i = 0; i < racers.Count; i++)
+            {
+                Racer r = racers[i];
+                
+                sw.WriteLine($"{r:A}");
+                sw.Write("Cars: ");
+                for (int j = 0; j < r.Cars.Length; j++)
+                {
+                    sw.Write(r.Cars[j] + "\t");
+                }
+                sw.WriteLine();
+                sw.Write("Years: ");
+                for (int k = 0; k < r.Years.Length; k++)
+                {
+                    sw.Write(r.Years[k] + "\t");
+                }
+                sw.WriteLine();
+            }
+            sw.Close();
+            FileStream fw = File.OpenRead("E:555.txt");
+            StreamReader sr = new StreamReader(fw);
+            SortedDictionary<int, string> sd = new SortedDictionary<int, string>();
+
+            while (true)
+            {
+                string s1 = sr.ReadLine();
+                string s2 = sr.ReadLine();
+                string s3 = sr.ReadLine();
+                if (s1 == null) break;
+                string name = s1.Substring(0, s1.IndexOf(","));
+                //Console.WriteLine(name);
+                for(int i=0;i<s3.Length;i++)
+                {
+                    if(s3[i]<='9'&&s3[i]>='0')
+                    {
+                        sd.Add(int.Parse(s3.Substring(i, 4)), name);
+                        i += 3;
+                    }
+                }
+            }
+            sr.Close();
+            foreach (var item in sd)
+
+            {
+
+                Console.WriteLine(item.Key + item.Value);
+
+            }
         }
+
     }
 }
